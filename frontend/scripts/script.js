f44b26872fd78c31ef36dc98d3e7a3ce60992f2b
vishal-menon <vishaljmenon@gmail.com>
2023-08-17 22:05:49 +0530

    Redesigned Frontend

--- frontend/scripts/script.js
+++ frontend/scripts/script.js
@@ -26,10 +26,14 @@ async function getUser(result) {
     try {
       const response = await axios.get(`http://localhost:3001/api/products/${result}`);
       document.getElementById('result').innerHTML = `
-    <h1>Product Information</h1>
-    <h1>Name : ${response.data.name}</h1>
-    <h1>Brand : ${response.data.brand}</h1>
-    <h1>Price : ${response.data.price}</h1>
+    <div class="productcard">
+    <div class="headingContainer">
+    <h1 class="cardHeading">Product Information</h1>
+    </div>
+    <h1 class="carddata">Name : ${response.data.name}</h1>
+    <h1 class="carddata">Brand : ${response.data.brand}</h1>
+    <h1 class="carddata">Price : ${response.data.price}</h1>
+    </div>
     `;
     } catch (error) {
       console.error(error);
